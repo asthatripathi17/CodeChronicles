b45a186c9710c99b92036457ba70a371c5c09159
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2023-10-08 12:07:51 +0530

    Update k_closest_elements.cpp

--- Day-7/k_closest_elements.cpp
+++ Day-7/k_closest_elements.cpp
@@ -1,1 +1,17 @@
+//using two pointer approach
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
 
+        int l = 0, h = arr.size() - 1;
+        while(h-l!=k-1){
+            if(abs(arr[l] - x)>abs(arr[h] - x)){
+                l++;
+            }
+            else{
+                h--;
+            }
+        }
+        return vector<int>(arr.begin()+l,arr.begin()+h+1);
+    }
+};
