33573bb5a6551c66c2534663e557e96ccae3b35b
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2023-10-10 23:16:34 +0530

    Update k_closest_elements.cpp

--- Day-7/k_closest_elements.cpp
+++ Day-7/k_closest_elements.cpp
@@ -16,3 +16,49 @@ public:
         return vector<int>(arr.begin()+l,arr.begin()+h+1);
     }
 };
+
+
+//Approach 2
+//using binary search + 2 pointer method
+class Solution {
+public:
+    int lowerBound(vector<int> arr, int x){
+        
+        int s = 0, e = arr.size()-1;
+        int ans = e;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] >= x) {
+                ans = mid;
+                e = mid-1;
+            } 
+            else {
+                s = mid + 1;
+            }
+        }
+    return ans;
+    }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+
+        int h = lowerBound(arr,x);
+        int l = h-1;
+        while(k>0){
+            if(l<0){
+                h++; //all closest elements will be on the high side
+            }
+            else if(h>arr.size()-1){
+                l--;
+            }
+            else if(abs(x-arr[l])>abs(arr[h]-x)){
+                h++;
+                //to increase the window size
+            }
+            else{
+                l--;
+            }
+            k--;
+        }
+        return vector<int>(arr.begin()+l+1, arr.begin()+h);
+    }
+};
