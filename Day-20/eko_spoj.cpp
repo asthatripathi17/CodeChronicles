06a4b4da02bd866a63fff166904ebe010677381d
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2023-10-25 23:47:40 +0530

    Update eko_spoj.cpp

--- Day-20/eko_spoj.cpp
+++ Day-20/eko_spoj.cpp
@@ -1,42 +1,42 @@
-//still not complete
 #include <iostream>
 #include <vector>
 #include <bits/stdc++.h>
 using namespace std;
-long long sumHeight(vector<int> arr, long long n, int sol){
-	long long ans = 0;
-	for(long long i = 0;i<n;i++){
+bool sumHeight(vector<long long int> arr, long long int sol, long long int m){
+	long long int ans = 0;
+	for(long long int i = 0;i<arr.size();i++){
 		if(arr[i]>sol){
 			ans+= (arr[i]-sol);
 		}
 	}
-	return ans;
+	return ans>=m;
 }
-int findHeight(vector<int> arr, long long n, long long m){
-	long long start = 0, end = *max_element(arr.begin(),arr.end());
+long long int findHeight(vector<long long int> arr, long long int m){
+	long long int ans = 0;
+	long long int start = 0, end = *max_element(arr.begin(),arr.end());
 	while(start<=end){
-		long long mid = start+(end-start)/2;
-		if(sumHeight(arr,n,mid)==m) return mid;
-		else if(sumHeight(arr,n,mid)>m){
+		long long int mid = start+(end-start)/2;
+		if(sumHeight(arr,mid,m)){
 			start = mid+1;
+			ans = mid;
 		}
 		else{
 			end = mid-1;
 		}
 	}
-	return -1;
+	return ans;
 }
 int main() {
 	// your code goes here
-	long long n,m;
+	long long int n,m;
 	cin>>n>>m;
-	int a = 0;
-	vector<int> arr;
-	for(int i = 0;i<n;i++){
+	long long int a = 0;
+	vector<long long int> arr;
+	for(long long int i = 0;i<n;i++){
 		cin>>a;
 		arr.push_back(a);
 	}
-	long long ans = findHeight(arr,n,m);
-	cout<<ans;
+	long long int ans = findHeight(arr,m);
+	cout<<ans<<endl;
 	return 0;
 }
