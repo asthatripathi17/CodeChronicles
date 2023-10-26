6b72df8be8ce8b80b221fb25c840604447a3ce93
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2023-10-27 01:43:09 +0530

    Update PRATA.cpp

--- Day-21/PRATA.cpp
+++ Day-21/PRATA.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 bool possibleSol(vector<int> rank, int p, int sol){
-    int currP = 0;
+    int currP = 0; //number of paratha made
     for(int i = 0; i<rank.size(); i++){
         int r = rank[i], noP = 1;
         int sumTime = 0;
@@ -12,7 +12,7 @@ bool possibleSol(vector<int> rank, int p, int sol){
             if(sumTime+noP*r<=sol){
                 currP++;
                 sumTime += noP*r;
-                noP++;
+                noP++; // for the arithmetic progression - 1R, 2R...noPR
             }
             else{
                 break;
@@ -27,7 +27,7 @@ bool possibleSol(vector<int> rank, int p, int sol){
 
 int minTime(vector<int> rank, int p){
     int n = rank.size(), ans = -1;
-    int start = 1, end = (rank[n-1]*p*(p+1)/2);
+    int start = 1, end = (rank[n-1]*p*(p+1)/2); //max element = most inefficient cook; this cook will take the most time if given all p
     while(start<=end){
         int mid = start+(end-start)/2;
         if(possibleSol(rank,p,mid)){
