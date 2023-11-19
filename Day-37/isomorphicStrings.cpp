628c658b8538ab3272b1dd29e997613d5b664371
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2023-11-19 08:12:22 +0530

    Update isomorphicStrings.cpp

--- Day-37/isomorphicStrings.cpp
+++ Day-37/isomorphicStrings.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        
-        unordered_map<char,char> umap;
+        if(s.size()!=t.size()) return false;
+
+        vector<int> s_map(256,0);
+        vector<bool> t_check(256,false);
+
         for(int i = 0; i<s.size(); i++){
-            if(umap.find(s[i]) == umap.end() && umap.find(t[i]) == umap.end()){
-                umap[s[i]] = t[i];
-                if(s[i] != t[i]) 
-                    umap[t[i]] = s[i];
+            if(s_map[s[i]] == 0){
+                s_map[s[i]] = t[i];
+                if(t_check[t[i]] == false){
+                    t_check[t[i]] = true;
+                }
+                else
+                    return false;
             }
-            else if(umap[s[i]] != t[i] || umap[t[i]] != s[i]){
-                cout<<s[i]<<" "<<t[i];
+            else if(s_map[s[i]] != t[i]){
                 return false;
             }
         }
+
         return true;
     }
 };
