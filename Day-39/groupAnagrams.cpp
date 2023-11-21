abc63fbf786fa8c661f40ca613b44a404ef20ffd
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2023-11-21 23:33:43 +0530

    Update groupAnagrams.cpp

--- Day-39/groupAnagrams.cpp
+++ Day-39/groupAnagrams.cpp
@@ -21,3 +21,30 @@ public:
 };
 
 //Approach 2 - Using hashmap
+class Solution {
+public:
+    
+    array<int,128> makeMap(string s){
+        array<int,128> hash = {0};
+        for(int i = 0; i<s.size(); i++){
+            hash[s[i]]++;
+        }
+        return hash;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<std::array<int,128>,vector<string>>hash_anagrams;
+        //not using umap as it requires hashing function
+        //for keys like array<int,256> we need custom hashing function
+        
+        for(int i = 0; i<strs.size(); i++){
+            hash_anagrams[makeMap(strs[i])].push_back(strs[i]);
+        }
+
+        vector<vector<string>> ans;
+        for(auto i:hash_anagrams){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
