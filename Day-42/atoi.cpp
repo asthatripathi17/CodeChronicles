586b2d910c74739b79897e3c8690e2af6a08b578
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2023-11-25 23:42:33 +0530

    Update atoi.cpp

--- Day-42/atoi.cpp
+++ Day-42/atoi.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
     bool checkDig(char c){
-
         return c>='0' && c<='9';
     }
-    int checkCase(long long int num, int sign){
+    int checkCase(long int num, int sign){
         num = num*sign;
         if(num<INT_MIN) return INT_MIN;
         else if(num>INT_MAX) return INT_MAX;
         
-        int ans = num;
-        return ans;
+        return int(num);
     }
     int myAtoi(string s) {
         int i = 0, sign = 1, once = 0, space = 0;
-        long long int num = 0;
+        long int num = 0;
         while(i<s.size()){
-            if(s[i] == ' ' && space!=1){
+            if(s[i] == ' ' && space!=1 && once!=1){
                 i++;
+                //can't have space after a number and sign
             }
             else if((s[i] == '+' || s[i] == '-') && once!=1){
                 if(s[i] == '-')
                     sign = -1;
                 i++;
-                once = 1;
+                once = 1; //use sign one time only
             }
             else if(!checkDig(s[i])){
                 return checkCase(num,sign);
             }
             else{
-                once = 1;
-                space = 1;
-                if(num<)
+                if(num*sign<INT_MIN) return INT_MIN;
+                else if(num*sign>INT_MAX) return INT_MAX;
+                
                 num = num*10 + s[i] - '0';
                 i++;
+                once = 1;
+                space = 1;
             }
         }
         return checkCase(num,sign);
