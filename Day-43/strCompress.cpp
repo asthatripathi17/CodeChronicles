334cf5491ba24173e64c05c41f6fd32498c1b5d4
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2023-12-06 01:30:10 +0530

    Update strCompress.cpp

--- Day-43/strCompress.cpp
+++ Day-43/strCompress.cpp
@@ -1,4 +1,4 @@
-//Space can be optimized by compressing the characters in-place within the original chars vector
+//Time-Optimized Approach
 class Solution {
 public:
     int compress(vector<char>& chars) {
@@ -38,6 +38,56 @@ public:
                 }
             }
         }
+        return chars.size();
+    }
+};
+
+//Space-optimized solution using in-place modifications
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        if(chars.size() == 1){
+            return chars.size();
+        }
+        //in place modifying
+        pair<char,int> count;
+        count.first = chars[0];
+        count.second = 1;
+
+        int i = 0;
+        while(i<chars.size()){
+            i++;
+            while(count.first == chars[i] && i<chars.size()){
+                count.second++;
+                chars.erase(i+chars.begin());
+            }
+            if(i<chars.size() && count.second>1 && count.first != chars[i]){
+                string str = to_string(count.second);
+                for(int j = str.size()-1; j>=0; j--){
+                    //if length >=10
+                    chars.insert(i+chars.begin(),str[j]);
+                }
+                //for a new char
+                i += str.size();
+                count.first = chars[i];
+                count.second = 1;
+            }
+            
+            if(i<chars.size() && count.first != chars[i]){
+                count.first = chars[i];
+                count.second = 1;
+            }
+            
+        }
+        //insert the count for last char
+        if(count.second>1){
+            string str = to_string(count.second);
+            for(int j = 0; j<str.size(); j++){
+                //if length >=10
+                chars.push_back(str[j]);
+            }
+        }
+        
         return chars.size();
     }
 };
