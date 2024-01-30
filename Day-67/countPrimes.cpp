8e5891f2a6e717285dc76a58aa3f321eb597199a
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2024-01-30 22:08:08 +0530

    Update countPrimes.cpp

--- Day-67/countPrimes.cpp
+++ Day-67/countPrimes.cpp
@@ -40,3 +40,30 @@ int countPrimes(int n) {
     }
     return count;
 }
+
+Approach 3
+
+// Using Optimized Sieve of Eratosthenes
+int countPrimes(int n) {
+    int count = n-2; //keep the count of primes encountered
+    
+    if(n<=1)    
+        return 0;
+    
+    vector<bool> sieve(n,true);
+    sieve[0] = false;
+    sieve[1] = false;
+    //Optimization 1
+    for(int i = 2; i*i<n; i++){
+        if(sieve[i]){
+            //Optimization 2
+            for(int j = i*i; j<n; j+=i){
+                if(sieve[j]){
+                    count--;
+                    sieve[j] = false;
+                }
+            }
+        }
+    }
+    return count;
+}
