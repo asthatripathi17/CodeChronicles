f509512e28f1603086b9cc0906bf97bcbe11912e
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2024-03-14 23:59:51 +0530

    Update minSizeSubarrSum.cpp

--- Day-83/minSizeSubarrSum.cpp
+++ Day-83/minSizeSubarrSum.cpp
@@ -16,3 +16,24 @@
         if(result == INT_MAX)   return 0;
         return result;
     }
+
+
+// Using a sliding window approach
+    int minSubArrayLen(int target, vector<int>& nums) {
+
+        int size = INT_MAX, L = 0, R = 0, sum = nums[R];
+        while(R<nums.size()){
+            if(sum<target){
+                R++;
+                if(R<nums.size())
+                    sum += nums[R];
+            }
+            else if(sum>=target && R<nums.size()){
+                size = min(size,R-L+1);
+                sum -= nums[L];
+                L++; // we got a new index to check for least window size which satisfies the condition
+            }
+        }
+
+        return size == INT_MAX?0:size;
+    }
