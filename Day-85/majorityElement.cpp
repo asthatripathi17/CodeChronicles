3935c0d792e6d83220030f08f918cef166160493
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2024-03-25 22:24:10 +0530

    Update majorityElement.cpp

--- Day-85/majorityElement.cpp
+++ Day-85/majorityElement.cpp
@@ -17,5 +17,16 @@ public:
         }
 
         return -1;
+
+        // Better Approach using Hashmap
+        unordered_map<int,int> frequency;
+        for(int i = 0;i<nums.size(); i++){
+            frequency[nums[i]]++;
+            if(frequency[nums[i]] > nums.size()/2){
+                return nums[i];
+            }
+        }
+        return -1;
+
     }
 };
