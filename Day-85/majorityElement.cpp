16545d7567660100f734f54880739d52c0a770dc
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2024-03-25 22:15:26 +0530

    Create majorityElement.cpp

---
+++ Day-85/majorityElement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        // Brute Force
+        int count = 0, major_element = 0;
+        for(int i = 0; i<nums.size(); i++){
+            count = 0;
+            for(int j = 0; j<nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+            if(count>nums.size()/2){
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
