edd2647627143c0daac810c95c653b7f2068506e
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2024-05-06 01:26:49 +0530

    Create plusOne.cpp

---
+++ Day-109/plusOne.cpp
@@ -0,0 +1,18 @@
+vector<int> plusOne(vector<int>& digits) {
+
+        for(int i = digits.size()-1; i>=0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            else if(digits[i] == 9){
+                if(i == 0){
+                    digits[i] = 0;
+                    digits.insert(digits.begin(), 1);
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+        }
+        return digits;
+    }
