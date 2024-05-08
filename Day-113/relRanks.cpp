9943714c62a6801bc84efd02eea9b6a4575a3369
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2024-05-08 23:16:28 +0530

    Update relRanks.cpp

--- Day-113/relRanks.cpp
+++ Day-113/relRanks.cpp
@@ -1,27 +1,26 @@
 vector<string> findRelativeRanks(vector<int>& score) {
-        map<int,string> store;
-        vector<int> output = score;
-        vector<string> res(score.size());
-        sort(output.begin(), output.end(), greater<int>());
-
+        
+        unordered_map<int,int> store;
         for(int i = 0; i<score.size(); i++){
+            store[score[i]] = i;
+        }
+        sort(score.begin(),score.end(),greater<int>());
+        vector<string> result(score.size());
+        for(int i = 0;i<score.size();i++){
+            int temp = store[score[i]];
             if(i == 0){
-                store[output[i]] = "Gold Medal";
+                result[temp] = "Gold Medal";
             }
             else if(i == 1){
-                store[output[i]] = "Silver Medal";
+                result[temp] = "Silver Medal";
             }
             else if(i == 2){
-                store[output[i]] = "Bronze Medal";
+                result[temp] = "Bronze Medal";
             }
             else{
-                store[output[i]] = to_string(i+1);
+                result[temp] = to_string(i+1);
             }
         }
-
-        for(int i = 0; i<score.size(); i++){
-            res[i] = store.find(score[i])->second;
-        }
         
-        return res;
-    }
+        return result;
+        }
