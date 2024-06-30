914e5b17d1611aae40d79d7a8975ca7337dbd8d9
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2024-06-30 21:57:05 +0530

    Update minCostTickets.cpp

--- Day-163/minCostTickets.cpp
+++ Day-163/minCostTickets.cpp
@@ -21,3 +21,32 @@ int solve(vector<int>& days, vector<int>& costs, int index){
 int mincostTickets(vector<int>& days, vector<int>& costs) {
     return solve(days,costs,0);
 }
+
+// Memoization Solution
+
+int solveMem(vector<int>& days, vector<int>& costs, int index, vector<int>& dp){
+    // Base Case
+    if(index>=days.size()){
+        return 0;
+    }
+    if(dp[index] != -1){
+        return dp[index];
+    }
+
+    int op1 = costs[0] + solveMem(days,costs,index+1,dp);
+
+    int i = index;
+    for(;i<days.size()&& days[i]<days[index]+7; i++);
+    int op2 = costs[1] + solveMem(days,costs,i,dp);
+
+    i = index;
+    for(;i<days.size()&& days[i]<days[index]+30; i++);
+    int op3 = costs[2] + solveMem(days,costs,i,dp);
+
+    dp[index] = min(op1,min(op2,op3));
+    return dp[index];
+}
+int mincostTickets(vector<int>& days, vector<int>& costs) {
+    vector<int> dp(days.size()+1,-1);
+    return solveMem(days,costs,0,dp);
+}
