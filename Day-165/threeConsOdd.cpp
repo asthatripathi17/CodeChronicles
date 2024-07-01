d07f430447702e35ed3f59043fc919ae085ebdaf
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2024-07-01 22:40:34 +0530

    Create threeConsOdd.cpp

---
+++ Day-165/threeConsOdd.cpp
@@ -0,0 +1,13 @@
+// Using bitwise operator & rather than using %
+bool threeConsecutiveOdds(vector<int>& arr) {
+    if(arr.size()<3){
+        return false;
+    }
+    for(int i = 0; i<arr.size()-2; i++){
+        if(arr[i]&1 && arr[i+1]&1 && arr[i+2]&1){
+            return true;
+        }
+    }
+
+    return false;
+}
