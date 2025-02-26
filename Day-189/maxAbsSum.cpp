7246433868c3a1aa5e2f940dd7ab5ee848e980ed
Astha Tripathi <121253696+asthatripathi17@users.noreply.github.com>
2025-02-26 23:38:39 +0530

    Create maxAbsSum.cpp

---
+++ Day-189/maxAbsSum.cpp
@@ -0,0 +1,13 @@
+int maxAbsoluteSum(vector<int>& nums) {
+    int ans = INT_MIN;
+    int maxSum = 0;
+    int minSum = 0;
+
+    for (int num : nums) {
+    maxSum = max(num, maxSum + num);
+    minSum = min(num, minSum + num);
+    ans = max({ans, maxSum, -minSum});
+    }
+
+    return ans;
+}
